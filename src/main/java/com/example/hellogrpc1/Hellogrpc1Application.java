d5d10af6cf2913df55dcb78cb81ff019d5187116
Swechha211 <bistaswechha@gmail.com>
2024-02-25 22:38:19 +0545

    basic completed

---
+++ src/main/java/com/example/hellogrpc1/Hellogrpc1Application.java
@@ -0,0 +1,13 @@
+package com.example.hellogrpc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellogrpc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellogrpc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellogrpc1/Hellogrpc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellogrpc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hellogrpc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
